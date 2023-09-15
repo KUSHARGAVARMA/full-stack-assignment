eeb7236ca6a0581e9925475781d27705b5d5883e
KUSHARGAVARMA <kushavrm008@gmail.com>
2023-09-15 22:51:53 +0530

    added cors for localhost testing, test the registration  route

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+// Enable CORS for all routes
+app.use(cors());
+app.use(express.json());
+
 
 const USERS = [];
 
@@ -16,21 +23,32 @@ const QUESTIONS = [{
 
 const SUBMISSION = [
 
-]
+]  
 
-app.post('/signup', function(req, res) {
-  // Add logic to decode body
-  // body should have email and password
+app.post('/signup', (req, res) => {
+  // Your signup logic here
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
 
+  // Your signup logic here
+  const {email, password}= req.body;
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
+  // const existingUser = USERS.find(user => user.email === email);
+  
+  // if (!existingUser) {
+  //   USERS.push({ email, password });
+  // }
+  console.log(req.body);
 
+  res.send(200);
+  
+  
+});
 
-  // return back 200 status code to the client
-  res.send('Hello World!')
-})
-
-app.post('/login', function(req, res) {
+// app.post('/login', function(req, res) {
   // Add logic to decode body
   // body should have email and password
 
@@ -43,26 +61,26 @@ app.post('/login', function(req, res) {
   // If the password is not the same, return back 401 status code to the client
 
 
-  res.send('Hello World from route 2!')
-})
+//   res.send('Hello World from route 2!')
+// })
 
-app.get('/questions', function(req, res) {
+// app.get('/questions', function(req, res) {
 
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
+  // res.send("Hello World from route 3!")
+// })
 
-app.get("/submissions", function(req, res) {
+// app.get("/submissions", function(req, res) {
    // return the users submissions for this problem
-  res.send("Hello World from route 4!")
-});
+  // res.send("Hello World from route 4!")
+// });
 
 
-app.post("/submissions", function(req, res) {
+// app.post("/submissions", function(req, res) {
    // let the user submit a problem, randomly accept or reject the solution
    // Store the submission in the SUBMISSION array above
-  res.send("Hello World from route 4!")
-});
+  // res.send("Hello World from route 4!")
+// });
 
 // leaving as hard todos
 // Create a route that lets an admin add a new problem
@@ -70,4 +88,4 @@ app.post("/submissions", function(req, res) {
 
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
-})
+});
